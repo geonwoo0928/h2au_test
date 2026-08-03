a78782077f6d0c332fc7dc655fac55d3def0acc3
geonwoo0928 <ddolikim6770@gmail.com>
2026-08-04 01:14:34 +0900

    [feature] 인공위성 사진 출력

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main() {
+  cv::Mat satellite_img = cv::imread("../satellite.png");
+  if(satellite_img.empty()) {
+      std::cerr << "위성 사진 불러들이기 실패!" << std::endl;
+      return -1;
+  }
+
+  cv::imshow("img", satellite_img);
+  cv::waitKey(0);
+
   return 1;
 }
